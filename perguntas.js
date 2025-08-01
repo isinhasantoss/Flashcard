d32d9d899f468d86d79291bd9f2f080a2d3ccf91
isinhasantoss <isabeli.luz.santos@escola.pr.gov.br>
2025-08-01 09:56:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é o Javascript?',
+    'O Javascript é uma linguagem de programação.'
+)
+
+criaCartao(
+ 'Geografia',
+ 'Qual é a capital do Paraná?',
+ ' capital do Paraná é Curitiba.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação.'
+)
